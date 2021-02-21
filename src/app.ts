0aa77b65368104c5411aa2043299c64e6e2c0a9f
Rafael Duarte <rafaeljfduarte@gmail.com>
2021-02-21 19:55:35 +0000

    feat: added styling capabilities to render

--- src/app.ts
+++ src/app.ts
@@ -2,37 +2,19 @@ class TemplateRender {
   templateEl: HTMLTemplateElement;
   targetEl: HTMLDivElement;
 
-  constructor(template: string, target: string) {
+  constructor(template: string, target: string, styling: string) {
     this.templateEl = document.getElementById(template)! as HTMLTemplateElement;
     this.targetEl = document.getElementById(target)! as HTMLDivElement;
 
     const node: DocumentFragment = document.importNode(this.templateEl.content, true);
 
-    this.append(node.firstElementChild as HTMLElement);
+    this.append(node.firstElementChild as HTMLElement, styling);
   }
 
-  private append(wrapper: HTMLElement) {
+  private append(wrapper: HTMLElement, styling: string) {
+    wrapper.id = styling;
     this.targetEl.insertAdjacentElement('afterbegin', wrapper);
   }
 }
 
-class ProjectInput {
-  templateEl: HTMLTemplateElement;
-  hostEl: HTMLDivElement;
-  element: HTMLElement;
-
-  constructor() {
-    this.templateEl = document.getElementById('project-input')! as HTMLTemplateElement;
-    this.hostEl = document.getElementById('app')! as HTMLDivElement;
-
-    const importedNode: DocumentFragment = document.importNode(this.templateEl.content, true);
-    this.element = importedNode.firstElementChild as HTMLElement;
-    this.attach();
-  }
-
-  private attach() {
-    this.hostEl.insertAdjacentElement('afterbegin', this.element)
-  }
-}
-
-const projectForm = new TemplateRender('project-input', 'app');
+const projectForm = new TemplateRender('project-input', 'app', 'user-input');
